66baf1c8f60dee2e6da89b33688da308c7a3399c
Danny182 <danny.zanchi@gmail.com>
2015-12-04 18:19:52 +0100

    Creati pacchetti focaccia e topping

---
+++ PizzeriaDiddieffe/src/pizzeriadiddieffe/core/focaccia/Focaccia.java
@@ -0,0 +1,8 @@
+package pizzeriadiddieffe.core.focaccia;
+
+import pizzeriadiddieffe.core.Item;
+
+public abstract class Focaccia implements Item{
+	public abstract String getInfo();	
+	public abstract double getPrice();
+}
